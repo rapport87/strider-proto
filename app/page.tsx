f8c16dad6a16f6821fe85d6daf0e816083f834e9
rapport87 <rapport87@gmail.com>
2024-07-22 23:38:15 +0900

    Jenkins CI/CD 테스트 원복

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,7 @@ export default function Home() {
     <div className="flex flex-col items-center justify-between min-h-screen p-6">
       <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
         <Image alt="women" src={women} width="150" height="150" />
-        <h2 className="text-2xl">
-          소금쟁이 가계부 프로토타입에 오신것을 환영합니다.
-        </h2>
+        <h2 className="text-2xl">소금쟁이 가계부에 오신것을 환영합니다</h2>
       </div>
       <div className="flex flex-col items-center gap-3 w-full">
         <Link href="/sign-up" className="primary-btn text-lg py-2.5">
