46c35ef547285c234763b26f6aa0ce1fb9209678
rapport87 <rapport87@gmail.com>
2024-07-22 23:32:04 +0900

    CI/CD 테스트

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
         <Image alt="women" src={women} width="150" height="150" />
         <h2 className="text-2xl">
-          소금쟁이 가계부 프로토타입에 오신것을 환영합니다
+          소금쟁이 가계부 프로토타입에 오신것을 환영합니다.
         </h2>
       </div>
       <div className="flex flex-col items-center gap-3 w-full">
